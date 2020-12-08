cbf76774805a2d415343113e077ebbc2afee65d6
Hayk <hayk.hayr3@gmail.com>
2020-12-08 23:45:28 +0400

    Add project files.

---
+++ Less05_task02/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Less05_task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+            Console.WriteLine(instance[6]);
+
+            Console.ReadKey();
+        }
+    }
+
+    class MyClass
+    {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+        }
+    }
+}
